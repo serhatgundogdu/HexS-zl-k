1f923481b245db4ec42ee2d6852847b79f929142
Serhat Gündoğdu <serhat.gundogdu@outlook.com>
2022-06-10 23:08:01 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ function App() {
   return (
     <Layout>
       <header className="flex items-center justify-center h-16 max-w-5xl px-4 py-4 mx-auto mt-12 text-black bg-transparent">
-        <h1 className="text-xl italic font-bold">HEXSozluk</h1>
+        <h1 className="text-2xl italic font-bold">HEX SÖZLÜK</h1>
       </header>
       <main className="justify-center w-full h-full ">
         {/* Search Box */}
-        <section className="h-full max-w-5xl pt-16 mx-auto text-center md:pt-3 ">
+        <section className="h-full max-w-5xl pt-3 mx-auto text-center ">
           <Search setSearch={setSearch} />
         </section>
-        <section className="h-full max-w-5xl pt-16 mx-auto text-center md:pt-3 ">
+        <section className="h-full max-w-5xl pt-3 mx-auto text-center ">
           {search.trim().length !== 0 && (
             <span className="flex">
               <h2 className="mr-1 font-semibold">{search}</h2> için arama
@@ -57,7 +57,7 @@ function App() {
             </span>
           )}
         </section>
-        <section className="h-full max-w-5xl pt-16 mx-auto text-center md:pt-1 ">
+        <section className="h-full max-w-5xl mx-auto text-center md:pt-1 ">
           <div className="grid grid-cols-1 gap-2 mt-4 md:mt-8 md:grid-cols-4">
             {words.length > 0 && words.map((word) => <Word word={word} />)}
           </div>
